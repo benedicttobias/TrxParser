8a058b195b2d1ffd03113a31bd1a8c806f587b34
Benedict Tobias <benedict.tobias@yahoo.com>
2020-04-25 12:44:26 -0400

    Finish returning hash of test result

--- Trx-Parser.psm1
+++ Trx-Parser.psm1
@@ -43,7 +43,7 @@ function ReadTestResult($TestResultFilePath){
 }
 
 function ProcessTestResultSummary {
-    $TestResultSummary = New-Object -TypeName psobject    
+    $TestResultSummary = New-Object -TypeName PsObject    
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name TrxFile -Value $TestResultFilePath
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name Outcome -Value $FileContent.TestRun.ResultSummary.outcome
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name Total -Value $FileContent.TestRun.ResultSummary.Counters.total
@@ -53,21 +53,28 @@ function ProcessTestResultSummary {
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name Timeout -Value $FileContent.TestRun.ResultSummary.Counters.timeout
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name Aborted -Value $FileContent.TestRun.ResultSummary.Counters.aborted
     $TestResultSummary | Add-Member -MemberType NoteProperty -Name Inconclusive -Value $FileContent.TestRun.ResultSummary.Counters.inconclusive
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name PassedButRunAborted -Value $FileContent.TestRun.ResultSummary.Counters.passedButRunAborted
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name NotRunnable -Value $FileContent.TestRun.ResultSummary.Counters.notRunnable
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name NotExecuted -Value $FileContent.TestRun.ResultSummary.Counters.notExecuted
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name Disconnected -Value $FileContent.TestRun.ResultSummary.Counters.disconnected
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name Warning -Value $FileContent.TestRun.ResultSummary.Counters.warning
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name Completed -Value $FileContent.TestRun.ResultSummary.Counters.completed
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name InProgress -Value $FileContent.TestRun.ResultSummary.Counters.inProgress
+    $TestResultSummary | Add-Member -MemberType NoteProperty -Name Pending -Value $FileContent.TestRun.ResultSummary.Counters.pending
 
     return $TestResultSummary
 }
 
 function ProcessTestSettings {
-    $TestSettings = New-Object -TypeName psobject    
+    $TestSettings = New-Object -TypeName PsObject    
     $TestSettings | Add-Member -MemberType NoteProperty -Name Name -Value $FileContent.TestRun.TestSettings.name
     $TestSettings | Add-Member -MemberType NoteProperty -Name Id $FileContent.TestRun.TestSettings.id
     $TestSettings | Add-Member -MemberType NoteProperty -Name Description $FileContent.TestRun.TestSettings.Description
 
     return $TestSettings
 }
-
 function ProcessTestTimes {
-    $TestTimes = New-Object -TypeName psobject    
+    $TestTimes = New-Object -TypeName PsObject    
     
     $StartTime = [datetime]::ParseExact($FileContent.TestRun.Times.start, "yyyy-MM-ddTHH:mm:ss.FFFFFFFK", $null)
     $EndTime = [datetime]::ParseExact($FileContent.TestRun.Times.finish, "yyyy-MM-ddTHH:mm:ss.FFFFFFFK", $null)
@@ -79,51 +86,64 @@ function ProcessTestTimes {
     return $TestTimes
 }
 
+function ProcessFailedTests {
+    # Failed test
+    $FailedTests = @()
+    foreach($UnitTestResult in $FileContent.SelectNodes('//ns:UnitTestResult[@outcome="Failed"]', $ns)) {
+        $FailedTest = New-Object -TypeName PsObject
+
+        $FailedTest | Add-Member -MemberType NoteProperty -Name ExecutionId -Value $UnitTestResult.executionId
+        $FailedTest | Add-Member -MemberType NoteProperty -Name Message -Value $UnitTestResult.Output.ErrorInfo.Message
+        $FailedTest | Add-Member -MemberType NoteProperty -Name StackTrace -Value $UnitTestResult.Output.ErrorInfo.StackTrace
+        
+        $CompleteStackTrace = $UnitTestResult.Output.ErrorInfo.Message + "`n" + $UnitTestResult.Output.ErrorInfo.StackTrace
+        $FailedTest | Add-Member -MemberType NoteProperty -Name CompleteStackTrace -Value $CompleteStackTrace
+
+        $FailedTests += ,@($FailedTest)
+    }
+ 
+    Write-Verbose "Match with unit test definition"
+    foreach($FailedTest in $FailedTests){
+        $XPath = "//ns:UnitTest/ns:Execution[@id='" + $FailedTest.ExecutionId + "']"
+        $TestDefinition = $FileContent.SelectNodes($XPath, $ns)
+        $CodeBase = $TestDefinition.NextSibling.codeBase
+        $TestName = $TestDefinition.NextSibling.name
+
+        $FailedTest | Add-Member -MemberType NoteProperty -Name CodeBase -Value $CodeBase
+        $FailedTest | Add-Member -MemberType NoteProperty -Name TestName -Value $TestName
+    }
+
+    return $FailedTests
+}
+
+function ProcessNotFailedTest {
+    # All test parser
+    $Results = @()
+    foreach($UnitTestResult in $FileContent.SelectNodes('//ns:UnitTestResult[@outcome!="Failed"]', $ns)) {
+        $Result = New-Object -TypeName PsObject
+
+        $Result | Add-Member -MemberType NoteProperty -Name ExecutionId -Value $UnitTestResult.executionId
+        $Result | Add-Member -MemberType NoteProperty -Name TestName -Value $UnitTestResult.testName
+        $Result | Add-Member -MemberType NoteProperty -Name Duration -Value $UnitTestResult.duration
+        $Result | Add-Member -MemberType NoteProperty -Name Outcome -Value $UnitTestResult.outcome
+
+        $Results += ,@($Result)
+    }
+ 
+    return $Results
+}
+
 function ProcessTestResult($FileContent){
     $ns = new-object Xml.XmlNamespaceManager $FileContent.NameTable
     $ns.AddNamespace("ns", $NameSpace)
 
-
-    # process test summary
-
-    # Process test times
-    ProcessTestTimes | Select-Object
-
-    # process test settings
-    ProcessTestSettings | Select-Object
-
-    # process test result summary
-    ProcessTestResultSummary | Select-Object
-
-
-    # # Failed test
-    # Write-Host "Getting failed test..."
-    # $FailedTests = @()
-    # foreach($UnitTestResult in $FileContent.SelectNodes('//ns:UnitTestResult[@outcome="Failed"]', $ns)) {
-    #     $testResult = New-Object -TypeName psobject
-
-    #     $testResult | Add-Member -MemberType NoteProperty -Name ExecutionId -Value $UnitTestResult.executionId
-    #     $testResult | Add-Member -MemberType NoteProperty -Name Message -Value $UnitTestResult.Output.ErrorInfo.Message
-    #     $testResult | Add-Member -MemberType NoteProperty -Name StackTrace -Value $UnitTestResult.Output.ErrorInfo.StackTrace
-        
-    #     $CompleteStackTrace = $UnitTestResult.Output.ErrorInfo.Message + "`n" + $UnitTestResult.Output.ErrorInfo.StackTrace
-    #     $testResult | Add-Member -MemberType NoteProperty -Name CompleteStackTrace -Value $CompleteStackTrace
-
-    #     $FailedTests += ,@($testResult)
-    # }
- 
-    # Write-Host "Match with unit test definition"
-    # foreach($FailedTest in $FailedTests){
-    #     $xpath = "//ns:UnitTest/ns:Execution[@id='" + $FailedTest.ExecutionId + "']"
-    #     $testDefinition = $FileContent.SelectNodes($xpath, $ns)
-    #     $codeBase = $testDefinition.NextSibling.codeBase
-    #     $testName = $testDefinition.NextSibling.name
-
-    #     $FailedTest | Add-Member -MemberType NoteProperty -Name CodeBase -Value $codeBase
-    #     $FailedTest | Add-Member -MemberType NoteProperty -Name TestName -Value $testName
-    # }
-
-    # return $FailedTests
+    return @{
+        TestSettings = ProcessTestSettings
+        TestTimes = ProcessTestTimes
+        TestResultSummary = ProcessTestResultSummary
+        FailedTests = ProcessFailedTests
+        NonFailedTests = ProcessNotFailedTest
+    }
 }
 
 
@@ -141,7 +161,7 @@ function Get-TestResult {
 
     Write-Verbose "I found that $TestResultFilePath to be the last file being written. I am going to use this."
     $FileContent = ReadTestResult($TestResultFilePath)
-    ProcessTestResult($FileContent)
+    return ProcessTestResult($FileContent)
 }
 
 Export-ModuleMember -Function Get-TestResult
